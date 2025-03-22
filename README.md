82ceca7851fd107b5bef4566bad8ed7bf9abacf4
Soham Bakshi <sohambakshi460@gmail.com>
2025-03-22 22:34:41 +0530

    1.0 (Basic code only works for prime modulus)

---
+++ README.md
@@ -0,0 +1,2 @@
+This code finds the simultaneous congruence of n congruences using brute force.
+Note: The initial code is not usable for non composite values of divisor
---
+++ simultaneous_congruence.py
@@ -0,0 +1,41 @@
+# Description: This program solves a system of simultaneous congruences
+
+n=int(input("Enter number of congruences:"))
+
+#Initialize the lists
+
+remainder=[]
+modulus=[]
+
+#Input the congruences
+
+for i in range (0,n,1):
+    remainder.append(int(input("Enter remainder:")))
+    modulus.append(int(input("Enter modulus:")))
+
+#Finding value of modulus of result
+
+p=1
+
+for i in range (0,n,1):
+    p=p*modulus[i]
+
+#Define the function of the system of congruences
+
+def func(a):
+    z=0
+    for j in range(0,n,1):
+        if (a-remainder[j])%(modulus[j])==0:
+            z+=1
+        else:
+            break
+    if z==n:
+        print("The solution is ",a,"mod(",p,")")
+        return True
+    return False
+
+#Finding the solution by brute force
+
+for i in range(0,p,1):
+    if func(i):
+        break
